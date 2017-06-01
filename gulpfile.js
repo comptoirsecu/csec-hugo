299cfe2d3c9a4283958579790e948fce744070e5
Astorsoft <astorz@gmail.com>
2017-06-01 21:59:15 +0200

    testing responsive pics

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var dest_css = "static/css";
+var css_dest = "static/css";
 var sass_src = "src/scss/app.scss";
 var runSequence = require('run-sequence');
 var javascript_src = ['src/js/enabled/foundation.core.js', 'src/js/enabled/foundation.util.*.js', 'src/js/enabled/*.js'];
-var dest_javascript = "static/js";
+var javascript_dest = "static/js";
+var covers_src = "src/images/covers/*.{png,jpg}";
+var covers_dest = "static/images/covers/";
+var pics_src = "src/images/misc/*.{png,jpg,gif}";
+var pics_dest = "static/images/misc/";
 var sassPaths = [
   'src/scss/foundation',
   'src/scss/normalize-scss/sass',
@@ -12,6 +16,80 @@ var sassPaths = [
 ];
 var exec = require('child_process').exec;
 
+
+gulp.task('covers:prod', function() {
+  return gulp.src(covers_src)
+  .pipe(plugins.responsive({
+    '*': [{
+        quality: 60,
+        rename: {
+          extname: '.jpg',
+          prefix: new Date().toISOString().split('T')[0] + '-'
+        }
+    },{
+      width: 320,
+      height: 180,
+      rename: {
+        prefix: new Date().toISOString().split('T')[0] + '-',
+        suffix: '-small',
+        extname: '.jpg',
+      },
+      // format option can be omitted because
+      // format of output image is detected from new filename
+      // format: 'jpeg'
+      // Do not enlarge the output image if the input image are already less than the required dimensions.
+      progressive: true,
+      quality: 60,
+    }, {
+      width: 640,
+      height: 360,
+      rename: {
+        prefix: new Date().toISOString().split('T')[0] + '-',
+        suffix: '-medium',
+        extname: '.jpg',
+      },
+      progressive: true,
+      quality: 60,
+    }, {
+      width: 1050,
+      height: 591,
+      rename: {
+        prefix: new Date().toISOString().split('T')[0] + '-',
+        suffix: '-large',
+        extname: '.jpg',
+      },
+      progressive: true,
+      quality: 60,
+    }, {
+      width: 640,
+      height: 360,
+      rename: {
+        prefix: new Date().toISOString().split('T')[0] + '-',
+        suffix: '-medium',
+        extname: '.webp',
+      },
+      quality: 50,
+    }, {
+      width: 1050,
+      height: 591,
+      rename: {
+        prefix: new Date().toISOString().split('T')[0] + '-',
+        suffix: '-large',
+        extname: '.webp',
+      },
+      quality: 50,
+    }],
+    }, {
+    // Global configuration for all images
+    // Strip all metadata
+    withMetadata: false,
+    embed: true,
+    background: "#000",
+    // Do not emit the error when image is enlarged.
+    errorOnEnlargement: false,
+  }))
+  .pipe(gulp.dest(covers_dest));
+});
 
 gulp.task('sass:dev', function() {
   return gulp.src(sass_src)
@@ -23,7 +101,7 @@ gulp.task('sass:dev', function() {
     .pipe(plugins.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest(dest_css));
+    .pipe(gulp.dest(css_dest));
 });
 
 gulp.task('sass:prod', function() {
@@ -36,7 +114,7 @@ gulp.task('sass:prod', function() {
     .pipe(plugins.autoprefixer({
       browsers: ['last 2 versions', 'ie >= 9']
     }))
-    .pipe(gulp.dest(dest_css));
+    .pipe(gulp.dest(css_dest));
 });
 
 
@@ -59,7 +137,7 @@ gulp.task('javascript:dev', function () {
   return gulp.src(javascript_src)
     .pipe(plugins.babel({ presets: ['es2015']}))
     .pipe(plugins.concat('app.js'))
-    .pipe(gulp.dest(dest_javascript));
+    .pipe(gulp.dest(javascript_dest));
 });
 
 gulp.task('javascript:prod', function () {
@@ -67,7 +145,7 @@ gulp.task('javascript:prod', function () {
     .pipe(plugins.babel({ presets: ['es2015']}))
     .pipe(plugins.concat('app.js'))
     .pipe(plugins.uglify())
-    .pipe(gulp.dest(dest_javascript));
+    .pipe(gulp.dest(javascript_dest));
 });
 
 gulp.task('html:prod', function() {
