7cb625c84f849dd9b2d435b10a0b3a8b669aaad0
Morgan Hotonnier <morgan.hotonnier@kindredgroup.com>
2018-04-12 19:18:37 +0200

    SRI, CSP et Duckduckgo
    Ajout des SRI partout ou c’est possible (en gros partout sauf podigee…)
    On gonfle un peu la CSP
    Et on dégage google search au profit de DuckDuckGo

--- static/js/google-search.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var cx = '007178495607479160844:u56cdtbbtki';
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
-  gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(gcse, s);
-})();
