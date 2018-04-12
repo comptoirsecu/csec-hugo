e88b40c48ef3402561b74cd6db206b9ecd0b4b8e
Morgan H <astorz@gmail.com>
2018-04-12 10:26:46 +0200

    Premier essai CSP

---
+++ static/js/google-search.js
@@ -0,0 +1,9 @@
+(function() {
+  var cx = '007178495607479160844:u56cdtbbtki';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
